774b600ada4a8bda24bfb9d365b4277770c0550b
Roman_Yavoriv <yavorco@mail.ru>
2017-06-28 09:20:39 +0300

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Trade or view your portfolio</h1>
+        <h6>You mas Save & Load your data</h6>
+        <h6>Click end day to begin new day</h6>
+        <hr>
+        <p>Your funds: {{funds}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            }
+        }
+    }
+</script>
